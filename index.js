490342a25b812d842654feaf638178e6582bd1c4
Berrouk-Salma <salmaberrouk4@gmail>
2025-04-23 16:36:47 +0100

    ✨ Add the style and 🃏 card components

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const objects = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
-const duplicatedObjects = [...objects]; 
+const duplicatedObjects = [...objects, ...objects]; 
 // console.log(duplicatedObjects);
-
 function jumble(array) {
     for(let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -11,16 +10,15 @@ function jumble(array) {
 
 jumble(duplicatedObjects);
 // console.log(duplicatedObjects);
-
-const mindGame = document.getElementById('Mind-game'); 
-duplicatedObjects.forEach(object => { 
+const mindGame = document.getElementById('Mind-game');
+duplicatedObjects.forEach(object => {
     const card = document.createElement('div');
     card.classList.add('card');
     card.dataset.card = object;
 
     const cardInner = document.createElement('div');
     cardInner.textContent = object;
-    cardInner.classList.add('card-inner'); 
+    cardInner.classList.add('card-inner');
 
     card.appendChild(cardInner);
     mindGame.appendChild(card);
